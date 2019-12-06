37a2576046806e25b60a69ae127edb340c3d3d9a
Vin Howe <24789592+iiridescent@users.noreply.github.com>
2019-12-06 15:41:23 -0700

    add piano information to and create inline 360 image viewer

--- server/index.php
+++ server/index.php
@@ -2,11 +2,14 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55118707-8"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
 
-  gtag('config', 'UA-55118707-8');
+        function gtag() {
+                dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-55118707-8');
 </script>
 <?php
 date_default_timezone_set('America/Denver');
@@ -16,42 +19,161 @@ $dayname = date('l, F j @ H:i');
 
 <head>
         <title>Open BYU classrooms today</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
         <style>
-		#hours {
-			overflow-x: scroll;		
-		}
+                body {
+                        margin: 0;
+                }
+
+                main {
+                        padding: 1.5em;
+                }
+
+                footer {
+                        padding: 0 1.5em 1.5em;
+                }
+
+                #hours {
+                        overflow-x: scroll;
+                }
 
                 #hours,
                 td,
                 td {
-                        border: 1px solid #aaa;
+                        border: 1px solid lightgray;
                         border-collapse: collapse;
                         padding: 10px;
                 }
 
-                .hour {
+                td {
+                        background-color: #f0f0f0;
+                }
+
+                .icon-cell {
                         text-align: center;
+                        background-color: transparent;
                 }
 
                 .deemphasized {
-			display: none;
+                        opacity: 0.75;
                 }
 
-                body {
-                        padding: 1.5em;
+                .piano {
+                        background-color: #ddFFdd;
+
                 }
 
-                html {
+                * {
                         font-family: "Lato", sans-serif;
                 }
+
+                .icon-cell a {
+                        text-decoration: none;
+                }
+
+                .button {
+                        background: linear-gradient(#fff, #eee);
+                        ;
+                        color: black;
+                        border: 1px solid darkgray;
+                        border-radius: 4px;
+                        padding: 4px;
+                        display: inline-block;
+                        transition: all 200ms;
+                        text-align: center;
+                        cursor: pointer;
+                }
+
+                .image-button {
+                        width: calc(100% - 10px);
+                }
+
+                .button:hover {
+                        background: #ccc;
+                }
+
+                .button:active {
+                        background: #999;
+                        border-color: #212121;
+                }
+
+                /* Based on https://html-online.com/articles/simple-popup-box/ */
+
+                /* Popup box BEGIN */
+                #hover-wrapper {
+                        background: rgba(0, 0, 0, .4);
+                        cursor: pointer;
+                        display: none;
+                        height: 100vh;
+                        position: fixed;
+                        text-align: center;
+                        top: 0;
+                        width: 100vw;
+                        z-index: 10000;
+                }
+
+                #hover-wrapper .hover-center {
+                        display: inline-block;
+                        height: 100%;
+                        vertical-align: middle;
+                }
+
+                #hover-wrapper>div {
+                        display: inline-block;
+                        height: 90%;
+                        max-width: 90%;
+                        min-height: 100px;
+                        vertical-align: middle;
+                        width: 90%;
+                        position: relative;
+                        border-radius: 4px;
+                }
+
+                #hover-wrapper>div>iframe {
+                        height: 100%;
+                        width: 100%;
+                        border-radius: 4px;
+                }
+
+                #popup-close-button {
+                        display: inline-block;
+                        /* font-weight: bold; */
+                        margin: 4px;
+                        position: absolute;
+                        right: 0;
+                        font-size: 25px;
+                        line-height: 30px;
+                        width: 30px;
+                        height: 30px;
+                }
+
+                /* Popup box END */
         </style>
 </head>
 
 <body>
+        <div id="hover-wrapper">
+                <span class="hover-center"></span>
+                <div>
+                        <iframe id="360-iframe" src="" frameborder="0"></iframe>
+                        <div id="popup-close-button" class="button">x</div>
+                </div>
+        </div>
         <main>
-                <h1>Open BYU classrooms for <?php echo "$dayname" ?> (MST)</h1>
-                <p>Use BYU's <a href="https://y.byu.edu/class_schedule/cgi/classRoom.cgi">Classroom Information</a> page to view more info about individual classrooms.</p>
+
+                <h1>Open BYU classrooms fall semester 2019 for <?php echo "$dayname" ?> (MST)</h1>
+                <p>Use BYU's <a href="https://y.byu.edu/class_schedule/cgi/classRoom.cgi" target="_blank">Classroom Information</a> page to view more info about individual classrooms.</p>
+                <p>Piano information may be incorrect for some classrooms. Use the 360° images to verify.</p>
+                <p>Classrooms are sorted (in order):</p>
+                <ol>
+                        <li>low-high by soonest open time</li>
+                        <li>high-low by length of the first scheduled range</li>
+                        <li>yes-no by whether there's a piano (remember to check)</li>
+                        <li>alphabetically by building name abbreviation</li>
+                        <li>alphanumerically by room number</li>
+                </ol>
+                <p>No guarantees are made on the accuracy of this information. Use BYU's <a href="https://y.byu.edu/ry/ae/prod/class_schedule/cgi/examDailySchedule.cgi" target="_blank">daily exam schedule</a> page to check when exams will be held in classrooms.</p>
+
                 <?php
                 $adjusted_day_of_week = $dayofweek - 1;
                 echo "<table id='hours'>\n\n";
@@ -122,12 +244,17 @@ $dayname = date('l, F j @ H:i');
 
                         let currentHourPos = (currentHour - 7) + minIndex;
 
-                        let aChildren = [].slice.call(a.children)
+
+                        let aArray = [].slice.call(a.children);
+                        let bArray = [].slice.call(b.children);
+                        let aChildren = aArray
                                 .filter(el => el.innerText == "False" || el.innerText == "True")
                                 .map(el => el.innerText == "True")
-                        let bChildren = [].slice.call(b.children)
+                        let bChildren = bArray
                                 .filter(el => el.innerText == "False" || el.innerText == "True")
                                 .map(el => el.innerText == "True")
+                        let aHasPiano = aArray[aArray.length - 1].innerText == "piano";
+                        let bHasPiano = bArray[bArray.length - 1].innerText == "piano";
                         let aRange = findFirstHourRangeInRow(currentHour, aChildren)
                         let bRange = findFirstHourRangeInRow(currentHour, bChildren)
 
@@ -151,6 +278,14 @@ $dayname = date('l, F j @ H:i');
                                 return -1;
                         }
 
+                        if (aHasPiano) {
+                                return -1;
+                        }
+
+                        if (bHasPiano) {
+                                return 1;
+                        }
+
                         // Otherwise, they start and end at the same time, and it's all the same
 
                         return 0;
@@ -158,11 +293,14 @@ $dayname = date('l, F j @ H:i');
         }
 
         window.onload = () => {
-		console.log("onload called")
+                console.log("onload called")
                 let currentDate = new Date()
                 let hours = currentDate.getHours()
                 let tableBody = document.querySelector('#hours').children[0]
                 let tableElements = [...tableBody.children]
+
+                // Example classroom image
+                // https://classroom-images-prd.s3.amazonaws.com/HFAC/a232t.jpg
 
                 // Algorithm should work as follows:
                 // Sorts by the one that is available now that has the most hours in the future available.
@@ -178,20 +316,53 @@ $dayname = date('l, F j @ H:i');
                 for (let element of tableElements) {
                         tableBody.appendChild(element)
                 }
-                // Not sure why this doesn't just work
-                Array.prototype.slice.call(document.querySelectorAll('td'))
-                        .filter(txt => txt.innerText.includes('True'))
-                        .forEach(txt => {
-                                txt.innerText = "✅";
-                                txt.className = "hour";
-                        });
-                [].slice.call(document.querySelectorAll('td'))
-                        .filter(txt => txt.innerText.includes('False'))
-                        .forEach(txt => {
-                                txt.innerText = "❌";
-                                txt.className = "hour";
-                        });
-                [].slice.call(document.querySelectorAll('td'))
+
+                let imageHeader = document.createElement('td')
+                imageHeader.innerText = "360° image"
+                tableElements[0].append(imageHeader)
+
+                for (let i = 1; i < tableElements.length; i++) {
+                        let imageCell = document.createElement('td')
+                        let imageLink = document.createElement('a')
+                        let bldgName = tableElements[i].children[0].innerText.toUpperCase();
+                        let roomName = tableElements[i].children[1].innerText.toLowerCase();
+
+                        imageLink.addEventListener("click", () => openPopup(bldgName, roomName));
+
+                        imageLink.className = "button image-button"
+                        imageLink.innerText = "📷"
+                        imageLink.href = '#'
+                        imageCell.className = "icon-cell";
+                        imageCell.append(imageLink)
+                        tableElements[i].append(imageCell)
+                }
+
+                let cells = [].slice.call(document.querySelectorAll('td'));
+                let trueCells = cells.filter(txt => txt.innerText.includes('True'))
+                let falseCells = cells.filter(txt => txt.innerText.includes('False'))
+
+                trueCells.forEach(txt => {
+                        txt.innerText = "✅";
+                        txt.className = "icon-cell";
+                });
+
+                falseCells.forEach(txt => {
+                        txt.innerText = "❌";
+                        txt.className = "icon-cell";
+                });
+
+                let pianoCells = cells.filter(txt => txt.innerText.includes('piano'))
+                let pia_noCells = cells.filter(txt => txt.innerText.includes('pia-no'))
+
+                pianoCells.forEach(txt => {
+                        txt.innerText = "🎹";
+                        txt.className = "icon-cell piano";
+                });
+
+                pia_noCells.forEach(txt => {
+                        txt.innerText = "";
+                        txt.className = "icon-cell piano";
+                });
 
                 // console.log(tableElements)
 
@@ -202,10 +373,23 @@ $dayname = date('l, F j @ H:i');
                         for (let i = 0; i < tableElements.length; i++) {
                                 let row = [...tableElements[i].children]
                                 for (let j = minIndex; j < maxIndex; j++) {
-                                        row[j].className = 'hour deemphasized'
+                                        row[j].className = 'icon-cell deemphasized'
                                 }
                         }
                 }
+
+                document.getElementById("popup-close-button").addEventListener("click", closePopup);
+        }
+
+        function closePopup() {
+                console.log('w')
+                document.getElementById('hover-wrapper').style.display = 'none';
+        }
+
+        function openPopup(bldg, room) {
+                let imageUrl = `http://${window.location.host}/360.html?bldg=${bldg}&room=${room}`
+                document.getElementById('360-iframe').src = imageUrl;
+                document.getElementById('hover-wrapper').style.display = 'initial';
         }
 </script>
 
