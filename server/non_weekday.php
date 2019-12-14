3e42c2a06e4c6e08b29fdfd2267aa21118a5e9b4
Vin Howe <24789592+iiridescent@users.noreply.github.com>
2019-12-14 15:56:52 -0700

    Change to use index.php controller and restrict website to view only during weekdays (this is a bug, not a feature)

---
+++ server/non_weekday.php
@@ -0,0 +1,6 @@
+<html>
+    <head><title>It's the weekend!</title></head>
+    <body>
+        <p>StudySpots only works on weekdays... for now. Email <a href="mailto:vin@studyspots.app">vin@studyspots.app</a> and I'll fix it.</p>
+    </body>
+</html>
